a20705b0760c0c7ebf67e47fb956719af9f58210
中川原 茂樹 <sh-nakagawara@kentem.co.jp>
2016-12-11 12:07:04 +0900

    ローカルに眠っていたアプリをGitHubへコミット

---
+++ ReviveDeletedFolder/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace ReviveDeletedFolder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.表示ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.終了ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "甦れフォルダー達よっ！！";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.表示ToolStripMenuItem,
+            this.終了ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(101, 48);
+            // 
+            // 表示ToolStripMenuItem
+            // 
+            this.表示ToolStripMenuItem.Name = "表示ToolStripMenuItem";
+            this.表示ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.表示ToolStripMenuItem.Text = "表示";
+            this.表示ToolStripMenuItem.Click += new System.EventHandler(this.表示ToolStripMenuItem_Click);
+            // 
+            // 終了ToolStripMenuItem
+            // 
+            this.終了ToolStripMenuItem.Name = "終了ToolStripMenuItem";
+            this.終了ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.終了ToolStripMenuItem.Text = "終了";
+            this.終了ToolStripMenuItem.Click += new System.EventHandler(this.終了ToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 60000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listView1
+            // 
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(728, 424);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            this.listView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listView1_KeyDown);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "フォルダー名";
+            this.columnHeader1.Width = 200;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "パス";
+            this.columnHeader2.Width = 500;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 424);
+            this.Controls.Add(this.listView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "甦れフォルダー達よっ！！";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 終了ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 表示ToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+
+    }
+}
+
---
+++ ReviveDeletedFolder/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using SHDocVw;
+using Shell32;
+
+namespace ReviveDeletedFolder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Interval = 60000; // 1分
+            //timer1.Interval = 1000;
+            timer1.Start();
+        }
+
+        private void Form1_Load( object sender, EventArgs e )
+        {
+            new Thread( new ThreadStart( HideForm ) ).Start();
+        }
+
+        private delegate void HideFormDelegate();
+
+        private void HideForm()
+        {
+            if ( InvokeRequired )
+            {
+                Invoke( new HideFormDelegate( HideForm ) );
+                return;
+            }
+            this.Hide();
+        }
+
+        private void 終了ToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            ApplicationExit();
+        }
+
+
+        private void ApplicationExit()
+        {
+            m_AppExit = true;
+
+            timer1.Stop();
+            notifyIcon1.Visible = false;
+            Application.Exit();
+        }
+        private bool m_AppExit = false;
+
+        private void timer1_Tick( object sender, EventArgs e )
+        {
+            var livingDirectories = new List<DirectoryManager>();
+
+            //COMのShellクラス作成
+            Shell shell = new Shell();
+
+            //IEとエクスプローラの一覧を取得
+            ShellWindows win = shell.Windows();
+
+            foreach ( IWebBrowser2 web in win )
+            {
+                try
+                {
+                    //エクスプローラのみ(IEを除外)
+                    if ( Path.GetFileName( web.FullName ).ToUpper() == "EXPLORER.EXE" )
+                    {
+                        string localPath = new Uri( web.LocationURL ).LocalPath;
+
+                        if ( !livingDirectories.Any( q => q.Name == web.LocationName ) )
+                            livingDirectories.Add( new DirectoryManager( web.LocationName, localPath ) );
+                    }
+                }
+                catch { }
+            }
+
+            // 死んでいったフォルダー達を弔う
+            foreach ( var lived in m_LivedDirectories )
+            {
+                if ( !livingDirectories.Any( q => q.Name == lived.Name ) )
+                {
+                    var dir = m_DeletedDirectories.FirstOrDefault(q => q.Name == lived.Name);
+                    if ( dir != null )
+                        m_DeletedDirectories.Remove( dir );
+
+                    m_DeletedDirectories.Insert( 0, new DirectoryManager( lived ) );
+                }
+            }
+
+            m_LivedDirectories = livingDirectories;
+        }
+
+        private List<DirectoryManager> m_LivedDirectories = new List<DirectoryManager>();
+        private List<DirectoryManager> m_DeletedDirectories = new List<DirectoryManager>();
+
+        private void 表示ToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            表示();
+        }
+
+        private void notifyIcon1_DoubleClick( object sender, EventArgs e )
+        {
+            表示();
+        }
+
+        private void 表示()
+        {
+            listView1.Items.Clear();
+
+            foreach ( var dir in m_DeletedDirectories )
+                listView1.Items.Add( new ListViewItem( new string[] { dir.Name, dir.Path } ) );
+
+            int ScreenWidth = Screen.PrimaryScreen.WorkingArea.Width;
+            int Screenheigth = Screen.PrimaryScreen.WorkingArea.Height;
+            int AppWidth = this.Width;
+            int AppHeight = this.Height;
+            int AppLeftXPos = ScreenWidth - AppWidth;
+            int AppLeftYPos = Screenheigth - AppHeight;
+            Rectangle tempRect = new Rectangle( AppLeftXPos, AppLeftYPos, AppWidth, AppHeight );
+            this.DesktopBounds = tempRect;
+
+            this.Show();
+        }
+
+        internal class DirectoryManager
+        {
+            public DirectoryManager(string name, string path)
+            {
+                Name = name;
+                Path = path;
+            }
+
+            public DirectoryManager( DirectoryManager dir )
+                : this( dir.Name, dir.Path )
+            {
+            }
+
+            public string Name { get; set; }
+            public string Path { get; set; }
+        }
+
+        private void Form1_FormClosing( object sender, FormClosingEventArgs e )
+        {
+            if ( !m_AppExit )
+            {
+                e.Cancel = true;
+                this.Hide();
+            }
+        }
+
+        private void listView1_DoubleClick( object sender, EventArgs e )        
+        {
+            ListView lvw = (ListView)sender;
+            if ( lvw != null )
+            {
+                var item = lvw.SelectedItems[0];
+                var ss = item.SubItems[1] as System.Windows.Forms.ListViewItem.ListViewSubItem;
+                string path = ss.Text;
+                if ( Directory.Exists( path ) )
+                {
+                    try { System.Diagnostics.Process.Start( path ); }
+                    catch
+                    {
+                        MessageBox.Show("フォルダーを開けませんでした。", "情報", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+        }
+
+        private void listView1_KeyDown( object sender, KeyEventArgs e )
+        {
+            switch(e.KeyData)
+            {
+                case Keys.F5:
+                    表示();
+                    break;
+            }
+        }
+    }
+}
